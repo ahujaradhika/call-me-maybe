3048ec01d2fc43a5846881831685420354824967
AashitaP <harshupatwari@gmail.com>
2019-07-20 18:51:57 -0700

    stuff

--- clicktocall/app.py
+++ clicktocall/app.py
@@ -6,32 +6,47 @@ from flask import url_for
 
 from twilio.twiml.voice_response import VoiceResponse
 from twilio.rest import Client
+
 import herepy
 import json
 import requests
 
-geocoderApi = herepy.GeocoderApi('rTc2ExgvmtXRk5fJG3IB', 'HwAH1Q70PPoaHB-hqswuHQ')
-geocoderReverseApi = herepy.GeocoderReverseApi('rTc2ExgvmtXRk5fJG3IB', 'HwAH1Q70PPoaHB-hqswuHQ')
-response = requests.get('https://places.cit.api.here.com/places/v1/autosuggest?at=37.787541999999995,-122.4109585&q=police&station&app_id=rTc2ExgvmtXRk5fJG3IB&app_code=HwAH1Q70PPoaHB-hqswuHQ')
 
-parsed = json.loads(response.text)
-
-url = parsed['results'][0]['href']
-response2 = requests.get(url)
-parsed2 = json.loads(response2.text)
-station_url = parsed2['results']['items'][0]['href']
-
-response3 = requests.get(station_url)
-parsed3 = json.loads(response3.text)
-phoneNumber = parsed3['contacts']['phone'][0]['value']
-print(phoneNumber)
-phoneNumber = '747-334-9366'
-print(phoneNumber)
+latitude='0'
+longitude='0'
 
 # Declare and configure application
 app = Flask(__name__, static_url_path='/static')
 app.config.from_pyfile('local_settings.py')
 
+geocoderApi = herepy.GeocoderApi('rTc2ExgvmtXRk5fJG3IB', 'HwAH1Q70PPoaHB-hqswuHQ')
+geocoderReverseApi = herepy.GeocoderReverseApi('rTc2ExgvmtXRk5fJG3IB', 'HwAH1Q70PPoaHB-hqswuHQ')
+
+# def get_phone_number():
+#     latitude = request.values['location_data[latitude]'] 
+#     longitude = request.values['location_data[longitude]']
+
+#     location_info = latitude + ',' + longitude
+#     print("LOCATION INFO")
+#     print(location_info)
+
+#     return location_info
+#     loc=latitude + ',' + longitude
+#     print("LOC")
+#     print(loc)
+#     response = requests.get('https://places.cit.api.here.com/places/v1/autosuggest?at='+loc+'&q=police&station&app_id=rTc2ExgvmtXRk5fJG3IB&app_code=HwAH1Q70PPoaHB-hqswuHQ')
+
+#     parsed = json.loads(response.text)
+
+#     url = parsed['results'][0]['href']
+#     response2 = requests.get(url)
+#     parsed2 = json.loads(response2.text)
+#     station_url = parsed2['results']['items'][0]['href']
+
+#     response3 = requests.get(station_url)
+#     parsed3 = json.loads(response3.text)
+#     phone_number = parsed3['contacts']['phone'][0]['value']
+#     return phone_number
 
 # Route for Click to Call demo page.
 @app.route('/')
@@ -45,7 +60,6 @@ def index():
 def call():
     # Get phone number we need to call
     phone_number = request.form.get('phoneNumber', None)
-
     try:
         twilio_client = Client(app.config['TWILIO_ACCOUNT_SID'],
                                app.config['TWILIO_AUTH_TOKEN'])
@@ -69,15 +83,42 @@ def call():
 def outbound():
     response = VoiceResponse()
 
-    response.say("Thank you for calling, we will redirect you to your selected personal.",
+    response.say("Thank you for calling, we will redirect you to the nearest support person",
                  voice='alice')
-    response.dial(phoneNumber)
+    response.dial('747-334-9366')
     '''
     # Uncomment this code and replace the number with the number you want
     # your customers to call.
     response.number("+16518675309")
     '''
     return str(response)
+
+@app.route('/receiver', methods = ['POST'])
+def worker():
+    #read json + reply
+    #data = request.form.get('location_data')
+    #print(data)
+    
+    #data = request.form.get('location_data')
+    latitude = request.values['location_data[latitude]'] 
+    longitude = request.values['location_data[longitude]']
+
+    location_info = latitude + ',' + longitude
+    loc=latitude + ',' + longitude
+    response = requests.get('https://places.cit.api.here.com/places/v1/autosuggest?at='+loc+'&q=police&station&app_id=rTc2ExgvmtXRk5fJG3IB&app_code=HwAH1Q70PPoaHB-hqswuHQ')
+
+    parsed = json.loads(response.text)
+
+    url = parsed['results'][0]['href']
+    response2 = requests.get(url)
+    parsed2 = json.loads(response2.text)
+    station_url = parsed2['results']['items'][0]['href']
+
+    response3 = requests.get(station_url)
+    parsed3 = json.loads(response3.text)
+    phone_number_outgoing = parsed3['contacts']['phone'][0]['value']
+    print("NEAREST POLICE STATION NUMBER")
+    print(phone_number_outgoing)
 
 
 # Route for Landing Page after Heroku deploy.
--- clicktocall/static/js/app.js
+++ clicktocall/static/js/app.js
@@ -1,4 +1,31 @@
 // Execute JavaScript on page load
+var location_data;
+
+window.onload = function() {
+navigator.geolocation.getCurrentPosition(function(location) {
+    var latitude = location.coords.latitude;
+    var longitude = location.coords.longitude;
+     console.log(longitude);
+    location_data = {"latitude": location.coords.latitude, "longitude": location.coords.longitude};
+    console.log(location_data)
+});    
+document.getElementById("theButton").onclick = function() {
+    doWork()
+};
+
+}
+
+function doWork() {
+
+    console.log("LOCATION DATA", location_data)
+    $.post("receiver", {"location_data": location_data}, function(){
+    });
+    // stop link reloading the page
+    event.preventDefault();
+    }
+
+
+
 $(function() {
     // Initialize phone number text input plugin
     $('#phoneNumber').intlTelInput({
@@ -6,11 +33,13 @@ $(function() {
         autoFormat: true,
         utilsScript: '/static/js/libphonenumber/src/utils.js'
     });
+        // setup the button click
 
     // Intercept form submission and submit the form with ajax
     $('#contactForm').on('submit', function(e) {
         // Prevent submit event from bubbling and automatically
         // submitting the form
+
         e.preventDefault();
 
         // Call our ajax endpoint on the server to initialize the
@@ -30,4 +59,33 @@ $(function() {
             alert(JSON.stringify(error));
         });
     });
+
+   /* var location_data
+    navigator.geolocation.getCurrentPosition(function(location) {
+        var latitude = location.coords.latitude;
+        var longitude = location.coords.longitude;
+        console.log(latitude);
+        console.log(longitude);
+        console.log("hellow");
+        location_data = {"latitude": location.coords.latitude, "longitude": location.coords.longitude};
+    });
+
+    $('#theButton').on('click', function(e) {
+        e.preventDefault();
+        console.log(location_data)
+        $.ajax({
+            url: '/receiver',
+            method: 'POST',
+            data: {
+                locationData: location_data
+            }
+        }).done(function(data) {
+            // The JSON sent back from the server will contain
+            // a success message
+            alert(data.message);
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        }); 
+
+    });*/
 });
